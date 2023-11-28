a3cc42c88a2c0dce5b1f922b007163acf232feff
N42k4 <default4175@gmail.com>
2023-11-28 13:48:11 +0700

    edit example dynamic type in ex01.js

--- javascript/ex01.js
+++ javascript/ex01.js
@@ -1,4 +1,14 @@
 let x = undefined;
 console.log(typeof x);
 x = String(x);
-console.log(typeof x)
+console.log(typeof x);
+
+let y = null;
+console.log(typeof y);
+y = String(y);
+console.log(typeof y);
+
+let z = true;
+console.log(typeof z);
+z = String(y);
+console.log(typeof z);
